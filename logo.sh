3f100bfd5f03bfbd38d539f2630ea0956f707cd3
Vikitoshi <31087501+vinskasenda@users.noreply.github.com>
2024-10-07 16:04:49 +0800

    Add files via upload

---
+++ logo.sh
@@ -0,0 +1,18 @@
+echo -e "\e[1;32m
+                          
+                          
+      db     \`7MMF'\`7MMF'
+     ;MM:       MM    MM  
+    ,V^MM.      MM    MM  
+   ,M  \`MM     MM    MM  
+   AbmmmqMA     MM    MM  
+  A'     VML    MM    MM  
+.AMA.   .AMMA..JMML..JMML.
+                          
+                          
+";
+echo -e "                 \033[48;2;9;10;12m Airdrop Insiders Indonesia \e[0m"; 
+echo -e "\e[0;37m Subscribe Our Telegram Channel: \e[4;35mhttps://t.me/AirdropInsiderID";
+echo -e "\e[0m"
+echo -e ""
+echo -e ""
